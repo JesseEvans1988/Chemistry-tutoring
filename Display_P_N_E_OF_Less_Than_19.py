8fee70042e4cc5f2e9f5d27a849a966db09e5e8d
JesseEvans1988 <34043510+JesseEvans1988@users.noreply.github.com>
2019-12-28 05:50:24 -0500

    Add files via upload

---
+++ Display_P_N_E_OF_Less_Than_19.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 05:03:12 2019
+
+@author: jesse
+"""
+
+print("Chose an element by entering it's atomic number. Remember the atomic number is the number of protons")
+c=int(input("Enter the number for your choice - "))
+
+if c==1:
+    print("You have selected Hydrogen. Hydrogen has 1 proton, 1 electron, and 0 neutrons." )
+elif c==2:
+    print("You have selected Helium. Helium has 2 protons, 2 electrons, and 2 neutrons. ")
+elif c==3:
+    print("You have selected Lithium. Lithium has 3 protons, 3 electrons, and 4 neutrons." )
+elif c==4:
+    print("You have selected Beryllium. Beryllium has 4 protons, 4 electrons, and 5 neutrons." )
+elif c==5:
+   print("You have selected Boron. Boron has 5 protons, 5 electrons, and 6 neutrons. ")
+elif c==6:
+    print("You have selected Carbon. Carbon has 6 protons, 6 electrons, and 6 neutrons. ")
+elif c==7:
+    print("You have selected Nitrogen. Nitrogen has 7 protons, 7 electrons, and 7 neutrons." )
+elif c==8:
+    print("You have selected oxygen. Oxygen has 8 protons, 8 electrons, and 8 neutrons." )
+elif c==9:
+   print("You have selected Fluorine. Fluorine has 9 protons, 9 electrons, and 10 neutrons. ")
+elif c==10:
+    print("You have selected Neon. Neon has 10 protons, 10 electrons, and 10 neutrons. ")
+elif c==11:
+    print("You have selected Sodiumium. Sodium has 11 protons, 11 electrons, and 12 neutrons." )
+elif c==12:
+    print("You have selected Magnesium. Magnesium has 12 protons, 12 electrons, and 12 neutrons." )
+elif c==13:
+   print("You have selected Aluminum. Aluminum has 13 protons, 13 electrons, and 14 neutrons. ")
+elif c==14:
+    print("You have selected Silicone. Silicon has 14 protons, 14 electrons, and 14 neutrons. ")
+elif c==15:
+    print("You have selected Phosphorous. Phosphorous has 15 protons, 15 electrons, and 16 neutrons.")
+elif c==16:
+    print("You have selected Sulfur. Sulfur has 16 protons, 16 electrons, and 16 neutrons.")
+elif c==17:
+   print("You have selected Chlorine. Chlorine has 17 protons, 17 electrons, and 18 neutrons.")
+elif c==18:
+   print("You have selected Argon. Argon is a Noble Gas with 18 protons, 18 electrons, and 22 neutrons.")
+else:
+    print("That is not a valid response. Please select another element with an atomic number less than 19. ")
+   
+   
